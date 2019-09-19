d7121170184fea85601df65f8c1a2888b29bc6bc
PF <pierre-francois.gimenez@laposte.net>
2019-09-20 00:16:02 +0200

    Fuzzer

--- fuzzer_api.ml
+++ fuzzer_api.ml
@@ -1,14 +1,10 @@
 open Base
-open Blind
-open Parser
-open Clean
-open Quotient
 
 let ()=
-    let grammaire = Parser.read_grammar_from_file Sys.argv.(1) in
-    let path = find_path_symbol grammaire [Terminal("homme"),[]] in
-    List.iter (fun r -> print_string ((regle2string r)^"\n")) path; flush stdout;
+    let grammaire = Base.read_bnf_grammar Sys.argv.(1) in
+    let path = find_path_symbol grammaire [Terminal(Sys.argv.(2)),[]] in
+(*    List.iter (fun r -> print_string ((regle2string r)^"\n")) path; flush stdout;*)
     let w = derive_with_path grammaire [[grammaire.axiome],path] in
-    print_string (partie2string w)
+    print_string ((partie2string w)^"\n")
 
 
