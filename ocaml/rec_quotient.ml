428823bca7b6e7f9deda5bd9725ff7f1a39af30a
PF <pierre-francois.gimenez@laas.fr>
2019-12-19 11:25:16 +0100

    New version of the inference engine (WIP)

--- ocaml/rec_quotient.ml
+++ ocaml/rec_quotient.ml
@@ -5,6 +5,8 @@ let extract_left_symbol (r : rule) : element option = match r.right_part with
     | [] -> None
     | t::_ -> Some(t)
 
+(* TODO: faire en sorte que le symbol en lui-même n'apparaisse pas dans la liste (sauf s'il est accessible récursivement *)
+
 (* get the last symbol of the rhs of a rule *)
 let extract_right_symbol (r : rule) : element option = match List.length r.right_part with
     | 0 -> None
@@ -27,9 +29,48 @@ let right_symbol_list (g: grammar) (task: element) : element list =
 let get_generative_rules (g: grammar) (e: element) : rule list =
     List.filter (fun r -> List.mem e r.right_part) g.rules
 
-let quotient_mem mem_size =
-    let mem : (ext_element, ext_rule list) Hashtbl.t = Hashtbl.create mem_size
+let quotient_mem (g: grammar) =
+    let mem_size = 100 in
+    let mem : ext_grammar = ext_grammar_of_grammar g
+    (* A -> [B,C] means that B, C, etc. can be prefix of derivations of A *)
+    (* if there is no key, prefix are unknown *)
     and rsym : (ext_element, element list) Hashtbl.t = Hashtbl.create mem_size
     and lsym : (ext_element, element list) Hashtbl.t = Hashtbl.create mem_size in
+
+    (* apply a quotient of a single rule with a prefix that is a single element *)
+    let quotient_by_one_element (pf: element) (r: ext_rule) : (ext_rule list) * (ext_element list)  =
+        let new_lhs = {pf=r.ext_left_symbol.pf@[pf]; e=r.ext_left_symbol.e; sf=r.ext_left_symbol.sf} in
+        match r.ext_right_part with
+        | [] -> [],[]
+        (* A -> aBC with prefix = a *)
+        | t::q when t.e=pf && is_ext_element_terminal t -> assert (t.pf=[] && t.sf=[]); [new_lhs ---> q],[]
+        (* A -> aBC with prefix != a *)
+        | t::q when is_ext_element_terminal t -> assert (t.pf=[] && t.sf=[]); [],[]
+        (* A -> BC with prefix = B *)
+        | t::q when t.e=pf && t.pf=[] -> let new_elem = {pf=[pf];e=t.e;sf=t.sf} in [new_lhs ---> q; new_lhs ---> (new_elem::q)],[new_elem]
+        (* A -> B_{D|}C *)
+        | t::q -> let possible = Hashtbl.find_opt lsym t in
+            if possible = None || List.mem pf (Option.get possible) then
+                let new_elem = {pf=t.pf@[pf];e=t.e;sf=t.sf} in [new_lhs ---> (new_elem::q)],[new_elem]
+            else
+                [],[]
+    in
+
+    (* apply a quotient of rules with a prefix *)
+    let rec quotient_rules (rlist: ext_rule list) (pf: part) (new_sym: ext_element list) : ext_element list = match pf with
+        | [] -> new_sym
+        | t::q -> let (new_rlist,new_new_sym) = List.split (List.map (quotient_by_one_element t) rlist) in
+            let new_rlist = List.flatten new_rlist and new_new_sym = List.flatten new_new_sym in
+            (quotient_rules [@tailcall]) new_rlist q (new_new_sym@new_sym)
+    in
+
+    (* compute the rules of a ext_element and do it recursively with every new symbol *)
+    let rec quotient_symbols (elist: ext_element list) (g: grammar) = match elist with
+        | [] -> ()
+        | t::q -> let new_elist = quotient_rules (List.map ext_rule_of_rule (get_generative_rules g t.e)) t.pf [] in
+            (quotient_symbols [@tailcall]) (new_elist@elist) g
+    in
+
         fun (g: grammar) (e: ext_element) : grammar -> g (* TODO *)
 
+let quotient (g : grammar) = quotient_mem g
