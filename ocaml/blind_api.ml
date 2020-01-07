869b700a01ba05994943528e9ade07f2fb38ae6b
PF <pierre-francois.gimenez@laposte.net>
2020-01-07 20:33:42 +0100

    Use Arg to parse the command line

--- ocaml/blind_api.ml
+++ ocaml/blind_api.ml
@@ -1,11 +1,29 @@
 let ()=
-    if Array.length Sys.argv = 7 then
-        let grammar = Grammar_io.read_bnf_grammar Sys.argv.(1)
-        and prefix = Grammar_io.read_tokens Sys.argv.(2)
-        and suffix = Grammar_io.read_tokens Sys.argv.(3)
-        and goal = List.hd (Grammar_io.read_tokens Sys.argv.(4))
-        and max_depth = int_of_string Sys.argv.(5)
-        and filename = Sys.argv.(6) in
+    let graph_fname = ref None in
+    let injg_fname = ref None in
+    let max_depth = ref 5 in
+    let grammar = ref None in
+    let prefix = ref None in
+    let suffix = ref None in
+    let goal = ref None in
+
+    let speclist = [
+        ("-maxdepth",   Arg.Set_int max_depth,    "Set the max depth search (default: "^(string_of_int !max_depth)^")");
+        ("-graph",      Arg.String (fun s -> graph_fname := Some(s)),    "Save the search graph");
+        ("-injg",       Arg.String (fun s -> injg_fname := Some(s)),     "Save the injection grammar");
+        ("-grammar",    Arg.String (fun s -> grammar := Some(Grammar_io.read_bnf_grammar s)),     "Save the injection grammar");
+        ("-pf",         Arg.String (fun s -> prefix := Some(Grammar_io.read_tokens s)),     "Prefix of the request");
+        ("-sf",         Arg.String (fun s -> suffix := Some(Grammar_io.read_tokens s)),     "Suffix of the request");
+        ("-goal",         Arg.String (fun s -> goal := Some(List.hd (Grammar_io.read_tokens s))),     "Terminal or nonterminal to reach")
+    ] in
+    let usage = "Error: grammar, prefix, suffix and goal are necessary" in
+    Arg.parse speclist ignore usage;
+
+    if !grammar <> None && !prefix <> None && !suffix <> None && !goal <> None then
+        let grammar = Option.get !grammar
+        and prefix = Option.get !prefix
+        and suffix = Option.get !suffix
+        and goal = Option.get !goal in
 
         let values = Hashtbl.create 100 in
         Hashtbl.add values (Grammar.Terminal("value")) "val1";
@@ -18,7 +36,7 @@ let ()=
 
         let fuzzer_oracle (g: Grammar.grammar) : bool = g |> fuzzer |> oracle in
 
-        let g = Blind.search fuzzer_oracle grammar goal max_depth (Some "test.dot") in match g with
+        let g = Blind.search fuzzer_oracle grammar goal !max_depth !graph_fname in match g with
         | None -> print_endline "No grammar found"
-        | Some(inj_g) -> print_endline ("Injection:  "^(Fuzzer.string_inst_of_part values (fuzzer (Grammar.grammar_of_ext_grammar inj_g)))); Grammar_io.export_bnf filename inj_g
-    else print_endline ("Usage : "^Sys.argv.(0)^" <input BNF filename> <prefix> <suffix> <goal> <max depth> <output BNF filename>")
+        | Some(inj_g) -> print_endline ("Injection:  "^(Fuzzer.string_inst_of_part values (fuzzer (Grammar.grammar_of_ext_grammar inj_g)))); Option.iter (fun f -> Grammar_io.export_bnf f inj_g) !injg_fname
+    else print_endline usage
