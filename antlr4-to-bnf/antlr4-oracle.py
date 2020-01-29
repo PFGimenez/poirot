91180507abf220e37207c6bf495e76471345f018
PF <pierre-francois.gimenez@laposte.net>
2020-01-29 23:39:28 +0100

    Use antlr4 as oracle (WIP)

--- antlr4-to-bnf/antlr4-oracle.py
+++ antlr4-to-bnf/antlr4-oracle.py
@@ -1,8 +1,26 @@
 from antlr4 import *
 import importlib
 import sys
+from antlr4.error.ErrorListener import ErrorListener
 
 # usage: grammar prefix suffix injection
+
+class MyErrorListener(ErrorListener):
+
+    def __init__(self):
+        super(MyErrorListener, self).__init__()
+
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        raise Exception
+
+    def reportAmbiguity(self, recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs):
+        raise Exception
+
+    def reportAttemptingFullContext(self, recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs):
+        raise Exception
+
+    def reportContextSensitivity(self, recognizer, dfa, startIndex, stopIndex, prediction, configs):
+        raise Exception
 
 def main():
     grammar = sys.argv[1]
@@ -19,7 +37,13 @@ def main():
     parser_name = sys.argv[1]+"Parser"
     parser_module = importlib.import_module(parser_name)
     parser = getattr(parser_module, parser_name)(stream)
-    parser.parse()
+    parser.addErrorListener(MyErrorListener())
+
+    try:
+        parser.parse()
+    except:
+        exit(1)
+    exit(0)
 
 if __name__ == '__main__':
     main()
